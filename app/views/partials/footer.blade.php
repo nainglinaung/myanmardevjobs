8b5e4edd47ca1069175f9b0c3cb394355c2d9365
Set Kyar Wa Lar <setkyar16@gmail.com>
2014-11-12 23:44:43 +0630

    Fix Footer Erros

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,10 +1,13 @@
-<div class="container">
-	<div class="footer">
-	  	<div class="col-md-6">
-	  		<p>&copy; <a href="https://twitter.com/setkyarwalar" target="_blank">Set Kyar Wa Lar</a> <?php echo date('Y'); ?></p>
-	  	</div>
-	  	<div class="col-md-6">	  		
-	  		<iframe style="border: medium none; overflow: hidden; width: 200px; height: 62px; margin-top: -20px;" class="pull-right" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fmyanmardevjobs&amp;width=100&amp;height=62&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=967913846556376" scrolling="no" allowtransparency="true" frameborder="0"></iframe>
-	  	</div>
-	</div>
+
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+	  			<p>&copy; <a href="https://twitter.com/setkyarwalar" target="_blank">Set Kyar Wa Lar</a> {{ date('Y') }}</p>
+	  		</div>	
+	  		<div class="col-md-6">	  		
+	  			<iframe style="border: medium none; overflow: hidden; width: 200px; height: 62px; margin-top: -20px;" class="pull-right" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fmyanmardevjobs&amp;width=100&amp;height=62&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=967913846556376" scrolling="no" allowtransparency="true" frameborder="0"></iframe>
+	  		</div>	
+		</div>
+    </div>
 </div>
