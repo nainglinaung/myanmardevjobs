de860ec5822dc1a8a21062dda7917b02901bc38c
setkyar <setkyar16@gmail.com>
2014-11-12 17:13:12 +0630

    Added Facebook Page

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -3,15 +3,8 @@
 	  	<div class="col-md-6">
 	  		<p>&copy; <a href="https://twitter.com/setkyarwalar" target="_blank">Set Kyar Wa Lar</a> <?php echo date('Y'); ?></p>
 	  	</div>
-	  	<div class="col-md-6">
-	  		<p class="pull-right">Follow on twitter! - Like us on FB</p>
-	  		<div class="pull-right">
-	  			Like us on Facebook : <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=967913846556376" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
-	  		</div>
-	  		<div class="pull-right">
-	  			Follow on Twitter : <a href="https://twitter.com/setkyarwalar" class="twitter-follow-button" data-show-count="false">Follow @setkyarwalar</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-	  		</div>
+	  	<div class="col-md-6">	  		
+	  		<iframe style="border: medium none; overflow: hidden; width: 200px; height: 62px; margin-top: -20px;" class="pull-right" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fmyanmardevjobs&amp;width=100&amp;height=62&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=967913846556376" scrolling="no" allowtransparency="true" frameborder="0"></iframe>
 	  	</div>
 	</div>
 </div>
