d62d14c7490035bfe17f5d5f8a88165e347153cb
setkyar <setkyar16@gmail.com>
2014-11-12 16:49:00 +0630

    Added Social Things

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -5,6 +5,13 @@
 	  	</div>
 	  	<div class="col-md-6">
 	  		<p class="pull-right">Follow on twitter! - Like us on FB</p>
+	  		<div class="pull-right">
+	  			Like us on Facebook : <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=967913846556376" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
+	  		</div>
+	  		<div class="pull-right">
+	  			Follow on Twitter : <a href="https://twitter.com/setkyarwalar" class="twitter-follow-button" data-show-count="false">Follow @setkyarwalar</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	  		</div>
 	  	</div>
 	</div>
 </div>
